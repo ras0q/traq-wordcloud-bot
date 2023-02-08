83fa402e83d8f0f81d0731c88af763351025536f
ras0q <ras0q@users.noreply.github.com>
2023-02-08 23:48:52 +0900

    :bug: allow nativa passwords

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -15,13 +15,14 @@ var (
 	HallOfFameChannelID = os.Getenv("TRAQ_HALL_OF_FAME_CHANNEL_ID")
 	JST                 = time.FixedZone("Asia/Tokyo", 9*60*60)
 	Mysql               = mysql.Config{
-		User:      os.Getenv("MARIADB_USERNAME"),
-		Passwd:    os.Getenv("MARIADB_PASSWORD"),
-		Net:       "tcp",
-		Addr:      fmt.Sprintf("%s:%d", os.Getenv("MARIADB_HOSTNAME"), 3306),
-		DBName:    os.Getenv("MARIADB_DATABASE"),
-		Collation: "utf8mb4_unicode_ci",
-		Loc:       JST,
-		ParseTime: true,
+		User:                 os.Getenv("MARIADB_USERNAME"),
+		Passwd:               os.Getenv("MARIADB_PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%d", os.Getenv("MARIADB_HOSTNAME"), 3306),
+		DBName:               os.Getenv("MARIADB_DATABASE"),
+		Collation:            "utf8mb4_unicode_ci",
+		Loc:                  JST,
+		AllowNativePasswords: true,
+		ParseTime:            true,
 	}
 )
