a9074720fc4910169e4d688bd37fcb291a4980be
ras0q <ras0q@users.noreply.github.com>
2023-02-09 00:40:10 +0900

    Revert ":boom: tmp get past wordcounts"
    This reverts commit d4734bed6fcca3b23e43c1b4d9f54871ce049006.

--- main.go
+++ main.go
@@ -18,24 +18,15 @@ func main() {
 	// cm := cron.Map{
 	// 	// daily wordcloud
 	// 	"10 0 * * *": func() {
+	today := time.Now().In(config.JST).AddDate(0, 0, -1)
 
-	// 一昨日以前のwordcountを取得
-	today := time.Now().In(config.JST).AddDate(0, 0, -2)
-	toyear := today.Year()
+	msgs, err := getDailyMessages(today)
+	if err != nil {
+		log.Println("[ERROR]", err)
+	}
 
-	for today.Year() == toyear {
-		today = today.AddDate(0, 0, -1)
-
-		msgs, err := getDailyMessages(today)
-		if err != nil {
-			log.Println("[ERROR]", err)
-		}
-
-		if err := postWordcloudToTraq(msgs, today); err != nil {
-			log.Println("[ERROR]", err)
-		}
-
-		log.Printf("Wordcloud for %s posted", today.Format("2006-01-02"))
+	if err := postWordcloudToTraq(msgs, today); err != nil {
+		log.Println("[ERROR]", err)
 	}
 	// 	},
 	// 	// yearly wordcloud
